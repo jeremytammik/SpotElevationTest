91fd6e695a32b767f575193a0cea352f0c15d072
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-09-28 08:11:29 +0200

    initial commit of code from keith

---
+++ SpotElevationTest/FamilySymbolWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+namespace SpotElevationTest
+{
+    /// <summary>
+    /// A wrapper of family symbol
+    /// </summary>
+    public class FamilySymbolWrapper
+    {
+        public string name { get; set; }
+        public string familyName { get; set; }
+        public string id { get; set; }
+        public string categoryName { get; set; }
+        public FamilySymbol familySymbol { get; set; }
+        public List<Parameter> parameters { get; set; }
+        public string attachmentPosition { get; set; }
+    }
+}
